75f01481bc82a78b7983e48bde3efce748cdc53b
HansaRasoga <hansarasoga@gmail.com>
2025-08-15 12:54:35 +0530

    Commit 1

---
+++ Binary Search Tree 2/Binary Search Tree Q2/ConNode.java
@@ -0,0 +1,15 @@
+public class ConNode 
+{
+String name;
+String phone;
+ConNode left;
+ConNode right;
+
+public ConNode(String name, String phone)
+{
+    this.name = name;
+    this.phone = phone;
+    this.left = null;
+    this.right = null;
+}
+}
